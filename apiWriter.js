e2ec77425e3bcfebaaedab48615d39077d9eae1e
German Escallon <germane@adtecinc.com>
2012-06-14 12:43:34 -0400

    Completed methods to create a new file-based application.

--- apiWriter.js
+++ apiWriter.js
@@ -15,9 +15,89 @@
  *************************************************************************/
 
 var 
+fs = require('fs'),
+https = require('https'),
+dataClient = require('./dataClient'),
+mime = require('mime'),
+
+encodeFieldHeader = function(boundary, name, value){
+   var result = [
+     "--" + boundary + "\r\n",
+     "Content-Disposition: form-data; name=\"", 
+     "" + name + "\"\r\n\r\n" + value + "\r\n",
+     ].join("");
+   return result;
+},
+
+encodeFileHeader = function(boundary, type, name, filename){
+   var result = [
+      "--" + boundary + "\r\n",
+      "Content-Disposition: form-data; name=\"" + name + "\"; filename=\"" + filename + "\"\r\n",
+      "Content-Type: " + type + "\r\n\r\n"
+     ].join("");
+   return result;
+},
+
+
+postMultipart = function(postData, boundary, apiCall){
+   var 
+   token = dataClient.getToken(),
+   len = 0, i=0,
+   options = null, 
+   request = null;
+   
+   if(!token){
+      console.log("Error: Unknown authentication token.");
+      return;    
+      }
+   
+   for( i=0; i<postData.length; i++){
+      len += postData[i].length;
+   }
+   
+   options = {
+      host: 'build.phonegap.com',
+      path: '/api/v1/' + apiCall + '?auth_token='+token,
+      method: 'POST',
+      headers:{
+         'Content-Type': 'multipart/form-data; boundary=' + boundary,
+         'Content-Length': len
+      }
+   };
+   
+   request = https.request(options, function(response){
+      response.on('data', function(chunk){
+         //TODO:  Do something w/ response data
+      })
+   });
+   
+   for( i=0; i<postData.length; i++){
+      request.write(postData[i]);
+   }
+   request.end(); 
+   
+},
+
+initMultipartUpload = function(inputFile, reqData, apiCall){
+   var  
+   boundary = 'bound' + Math.random(),
+   postData = [], 
+   fileReader = null,
+   fileContents = '';
+   
+   postData.push(new Buffer(encodeFieldHeader(boundary, "data", JSON.stringify(reqData)), 'ascii'));
+   postData.push(new Buffer(encodeFileHeader(boundary, mime.lookup(inputFile), "file", inputFile), 'ascii'));
+   fileReader = fs.createReadStream(inputFile, {encoding: 'binary'});
+   fileReader.on('data', function(fileData){ fileContents+= fileData;});
+   fileReader.on('end', function(){
+      postData.push(new Buffer(fileContents, 'binary'));
+      postData.push(new Buffer("\r\n--" + boundary + "--", 'ascii'));
+      postMultipart(postData, boundary, apiCall);
+      });
+},
 
 createApp = function(inputFile, dataObj){
-  
+   initMultipartUpload(inputFile, dataObj, 'apps');
 },
 
 updateAppFromFile = function(inputFile, appId){
